75dcc304a8859adf6ac6a5c199176d84695241b9
王于晟 <wangys2@fruitday.com>
2018-04-12 14:07:51 +0800

    update node

--- k8s_install.sh
+++ k8s_install.sh
@@ -4,4 +4,5 @@ ansible-playbook -i hosts 00-init.yml && \
 ansible-playbook -i hosts 01-ssl.yml && \
 ansible-playbook -i hosts 02-etcd.yml && \
 ansible-playbook -i hosts 03-docker.yml && \
-ansible-playbook -i hosts 04-k8s-master.yml
+ansible-playbook -i hosts 04-k8s-master.yml && \
+ansible-playbook -i hosts 05-k8s-node.yml 
